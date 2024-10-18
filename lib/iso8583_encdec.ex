7d26e6df13e27c45bcdc6800f6d8a51cbc7bc6b1
Mohd Norhaimi bin Yahya <haimiyahya@gmail.com>
2024-10-18 09:26:48 +0800

    init commit, just initialized the project.

---
+++ lib/iso8583_encdec.ex
@@ -0,0 +1,18 @@
+defmodule Iso8583Encdec do
+  @moduledoc """
+  Documentation for `Iso8583Encdec`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Iso8583Encdec.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/iso8583_encdec_test.exs
@@ -0,0 +1,8 @@
+defmodule Iso8583EncdecTest do
+  use ExUnit.Case
+  doctest Iso8583Encdec
+
+  test "greets the world" do
+    assert Iso8583Encdec.hello() == :world
+  end
+end
