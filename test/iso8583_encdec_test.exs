756654fcc5571df61a18d6955c8207169d0847fe
Mohd Norhaimi bin Yahya <haimiyahya@gmail.com>
2024-10-22 10:56:55 +0800

    added new test

--- test/iso8583_encdec_test.exs
+++ test/iso8583_encdec_test.exs
@@ -1,9 +1,8 @@
 defmodule Iso8583EncdecTest do
   use ExUnit.Case
   doctest Iso8583Encdec
-  alias BitmapBinaryHeaderBcd, as: Iso8583Parser
 
-  test "parse f63 and f64" do
+  test "parse f 35, f63 and f64" do
     bmp = "0000000020000003"
     f35 = "35123456789012345678901234567890123450"
     f63 = "06123456"
@@ -12,7 +11,7 @@ defmodule Iso8583EncdecTest do
     msg = bmp <> f35 <> f63 <> f64
 
     msg = Base.decode16!(msg)
-    parsed_msg = Iso8583Parser.parse_msg(msg)
+    parsed_msg = BitmapBinaryHeaderBcd.parse_msg(msg)
 
     assert Map.has_key?(parsed_msg, 63)
     assert Map.get(parsed_msg, 63) == "12"
@@ -20,9 +19,42 @@ defmodule Iso8583EncdecTest do
     assert Map.has_key?(parsed_msg, 64)
     assert Map.get(parsed_msg, 64) == "5678"
 
-    IO.inspect(parsed_msg)
+  end
+
+  test "parse alphanum field data_type with 0 digits bcd header, 2 digits bcd header and 3 digits bcd header" do
+    bmp = "0000000000000007"
+    f62 = "35363738"
+    f63 = "0435363738"
+    f64 = "000435363738"
+
+    msg = bmp <> f62 <> f63 <> f64
+    msg = Base.decode16!(msg)
+
+    parsed_msg = BmpBinHeaderBcdDataNumBcd_AlphanumDataType.parse_msg(msg)
+
+    assert Map.get(parsed_msg, 62) == "5678"
+    assert Map.get(parsed_msg, 63) == "5678"
+    assert Map.get(parsed_msg, 64) == "5678"
 
   end
+
+  test "parse numeric field data_type with 0 digits bcd header, 2 digits bcd header and 3 digits bcd header" do
+    bmp = "0000000000000007"
+    f62 = "5678"
+    f63 = "045678"
+    f64 = "00045678"
+
+    msg = bmp <> f62 <> f63 <> f64
+    msg = Base.decode16!(msg)
+
+    parsed_msg = BmpBinHeaderBcdDataNumBcd_NumericDataType.parse_msg(msg)
+
+    assert Map.get(parsed_msg, 62) == "5678"
+    assert Map.get(parsed_msg, 63) == "5678"
+    assert Map.get(parsed_msg, 64) == "5678"
+
+  end
+
 end
 
 
@@ -34,5 +66,27 @@ defmodule BitmapBinaryHeaderBcd do
   define(35, "z.. 39")
   define(63, "n.. 2")
   define(64, "an 4")
+
+end
+
+defmodule BmpBinHeaderBcdDataNumBcd_AlphanumDataType do
+  use Iso8583Dec, header_encoding: :bcd,
+    numeric_encoding: :bcd,
+    bitmap_format: :bin
+
+  define(62, "an 4")
+  define(63, "an.. 4")
+  define(64, "an... 4")
+
+end
+
+defmodule BmpBinHeaderBcdDataNumBcd_NumericDataType do
+  use Iso8583Dec, header_encoding: :bcd,
+    numeric_encoding: :bcd,
+    bitmap_format: :bin
+
+  define(62, "n 4")
+  define(63, "n.. 4")
+  define(64, "n... 4")
 
 end
